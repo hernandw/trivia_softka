7df8c478b55434e882de91f6496de80ee429d452
hernandw <hernandw@gmail.com>
2021-12-24 11:18:27 -0300

    elaboracion de preguntas

---
+++ data/question.js
@@ -0,0 +1,27 @@
+ export const data = [
+  {
+    question: "¿Quién escribió 'La Ilíada'?",
+    choices: ["Marco Tulio Ciceron", "Homero", "Séneca", "Herodoto"],
+    answer: choices[1]
+  },
+  {
+    question: "Autor de 'El arte de la guerra'",
+    choices: ["SHi Jing", "Dao De Jing", "Sun Tzu", "Confucio"],
+    answer: choices[2]
+  },
+  {
+    question: "¿Quién es el autor de la 'Divina comedia'?",
+    choices: ["Dante Alighieri", "Petrarca", "Giovanni Bocaccio", "Maquiavelo"],
+    answer: choices[0]
+  },
+  {
+    question: "Filosofo griego, autor de 'La República'",
+    choices: ["Sócrates", "Aristóteles", "Democrates", "Platon"],
+    answer: choices[3]
+  },
+  {
+    question: "¿Por qué El Principito decide abandonar su planeta?",
+    choices: ["quería conocer la tierra", "Estaba enfermo", "El rey le ordena irse", "una rosa se aprovechaba de él"],
+    answer: choices[0]
+  },
+];
